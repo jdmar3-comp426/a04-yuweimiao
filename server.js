f93abd69b26ddce23d4e7e86d119c28e821bd110
Rong yun <rongyun@RongdeMacBook-Air.local>
2021-11-29 04:00:43 -0500

    database

--- server.js
+++ server.js
@@ -2,15 +2,15 @@
 var express = require("express")
 var app = express()
 // Require database SCRIPT file
-
+var db = require("./database.js");
 // Require md5 MODULE
-
+var md5 = require("md5");
 // Make Express use its own built-in body parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set server port
-
+var HTTP_PORT = 5000;
 // Start server
 app.listen(HTTP_PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
@@ -31,13 +31,16 @@ app.get("/app/users", (req, res) => {
 });
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
-
+app.get("/app/users", (req,res) => {
+	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = 2").all();
+	res.status(200).json(stmt);
+});
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 
 // Default response for any other request
 app.use(function(req, res){
-	res.json({"message":"Endpoint not found. (404)"});
+	res.json({"message":"Your API is working"});
     res.status(404);
 });
